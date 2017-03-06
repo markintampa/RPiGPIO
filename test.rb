f4b0e724d0cea541f102aed8ee0bb3b7b1e02a35
Mark <mmniece@gmail.com>
2017-03-06 22:31:08 +0000

    Inputs working

--- test.rb
+++ test.rb
@@ -6,13 +6,13 @@ RPi::GPIO.set_numbering :bcm
 
 ON = true
 OFF = false
-IN1 = 12
-IN2 = 13
+IN1 = 19
+IN2 = 26
 PINS = [4, 17, 6]
 @state = [OFF, OFF, OFF]
 
-RPi::GPIO.setup IN1, :as => :input, :pull => :down
-RPi::GPIO.setup IN2, :as => :input, :pull => :down
+RPi::GPIO.setup IN1, :as => :input, :pull => :up
+RPi::GPIO.setup IN2, :as => :input, :pull => :up
 
 PINS.each do |p|
   RPi::GPIO.setup p, :as => :output
@@ -49,26 +49,32 @@ end
 
 def pi_loop
   loop do
-    pin_1 = RPi::GPIO.high? IN1
-    pin_2 = RPi::GPIO.high? IN2
+    pin_1 = RPi::GPIO.low? IN1
+    pin_2 = RPi::GPIO.low? IN2
     @index ||= 0
     @count ||= 0    
     @count += 1
     system("clear")
     puts "Waiting for switch input...#{@count} Pin 1 - #{pin_1} | Pin 2 - #{pin_2}"
-    
+
+    @prev_index = @index    
+
     if pin_1 && pin_2
       break
     elsif pin_1
       puts "up"
       @index += 1
-      @index = 0 if @index > 3
+      @index = 0 if @index >= PINS.count
     elsif pin_2
       puts "down"
       @index -= 1
-      @index = 3 if @index < 0
+      @index = 2 if @index < 0
     end
-    sleep 0.5
+
+    RPi::GPIO.set_low PINS[@prev_index]
+    RPi::GPIO.set_high PINS[@index]
+
+    sleep 0.1
   end
 end
 
